84b6add1c7d82199c20206dcfd40e40d47ca1409
sfinnie <scott.finnie@roperia.com>
2016-06-10 23:11:29 +0100

    initial, sequential java implementation

--- java/ConComp.java
+++ java/ConComp.java
@@ -1,5 +1,28 @@
+
+import java.util.HashMap;
+import java.io.File;
+
+
 public class ConComp {
 	public static void main(String[] args) {
 		System.out.println("Number of command line parameters: " + args.length);
+		
+		java.util.HashMap<String, Long> lengths = new java.util.HashMap<>();
+		for(String fname: args) {
+			File file = new File(fname);
+			lengths.put(fname, file.length());
+		}
+		
+		String longest="";
+		long maxlen=0;
+		for(String fname: lengths.keySet()) {
+			if(lengths.get(fname) > maxlen) {
+				maxlen = lengths.get(fname);
+				longest = fname;
+			}
+		}
+		
+		System.out.println("File " + longest + " is the biggest at " + maxlen + " bytes");
+		
 	}
 }
