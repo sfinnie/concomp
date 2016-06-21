3e38c0a59a133ded4e29ece846dcb4e2db31c9e5
Scott Finnie <scott.finnie@roperia.com>
2016-06-21 16:09:21 +0100

    initial c sharp implementation using .Net core

---
+++ csharp/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fname1 = args[0];
+            var fname2 = args[1];
+            var task1 = Task<long>.Factory.StartNew(() => new FileInfo(fname1).Length);
+            var task2 = Task<long>.Factory.StartNew(() => new FileInfo(fname2).Length);
+
+            Task.WaitAll(task1, task2);
+
+            if (task1.Result > task2.Result)
+            {
+                Console.WriteLine($"{fname1} is bigger");
+            }
+            else if (task2.Result > task1.Result)
+            {
+                Console.WriteLine($"{fname2} is bigger");
+            }
+            else
+            {
+                Console.WriteLine("The files are the same size");
+            }
+        }
+    }
+}
---
+++ csharp/README.md
@@ -0,0 +1,15 @@
+#C Sharp implementation of ConComp
+
+##To run:
+
+1. Install [.Net Core](https://www.microsoft.com/net/core#windows) if you haven't already.  This is the command line C# toolset and is cross-platform (Linux/Mac/Windows).
+2. clone this repo
+3. Restore the dependencies and build the solution: 
+        ~/projects/concomp/csharp$ dotnet restore
+        ~/projects/concomp/csharp$ dotnet build
+4. Run the program: 
+        ~/projects/concomp/csharp$ dotnet .\bin\debug\netcoreapp1.0\csharp.dll file1.txt file2.txt file3.txt
+
+
+
+
