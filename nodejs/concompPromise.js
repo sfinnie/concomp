49a33075ea423b3b1b37bb7f826e49cc855e68e2
Scott Finnie <scott.finnie@roperia.com>
2016-06-21 09:17:49 +0100

    add Promise-based javascript solution

--- nodejs/concompPromise.js
+++ nodejs/concompPromise.js
@@ -1,47 +1,20 @@
-var fs = require('fs');
-var Promise = require('promise');
+'use strict';
+
+const co = require('co');
+const fs = require('fs-promise');
 
 var fname1=process.argv[2]
 var fname2=process.argv[3]
-var size1 = undefined
-var size2 = undefined
 
-console.log("\n~~~~~ Start ~~~~~\n")
-console.log("1. Creating promises to obtain file sizes... ")
-p1=fileSize(fname1)
-p2=fileSize(fname2)
-console.log("2. Promises created.")
-
-console.log("3. Waiting on promises to complete... ")
-p1.then((size) => console.log("    3.1. Size of file " + fname1 + ": " + size)) 
-p1.then((size) => size2 = size) 
-p2.then((size) => console.log("    3.1. Size of file " + fname2 + ": " + size)) 
-console.log("4. Promises Resolved. ")
-
-console.log("5. Comparing file sizes...")
-if(size1 > size2) {
-	console.log("    The first file is bigger")
-} else if (size2 > size1) {
-	console.log("    The second file is bigger")
-} else {
-	console.log("    The files are the same size")
-}
-console.log("6. Comparison complete.")
-console.log("\n~~~~~ End ~~~~~\n")
-
-function fileSize(fname) {
-	return new Promise(function (resolve, reject) {
-		fs.stat(fname, function doneReading(err, stats) {
-			if (err) {
-				reject(err)
-			} else {
-				resolve(stats["size"])
-			}
-		})
-	})
-}
-
-
-
-
-
+co(function* () {
+    let res = yield [fs.stat(fname1), fs.stat(fname2)];
+	let size1 = res[0]["size"]
+	let size2 = res[1]["size"]
+	if(size1 > size2) {
+		console.log(fname1 + " is bigger")
+	} else if (size2 > size1) {
+		console.log(fname2 + " is bigger")
+	} else {
+		console.log("The files are the same size")
+	}
+})
